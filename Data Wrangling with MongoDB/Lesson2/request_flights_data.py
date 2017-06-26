323008f1f776a2c8862701aa1d5d99fdd865b3aa
Qiang Lu <buaaluqiang@hotmail.com>
2017-06-26 13:30:55 -0600

    make request_flights_y to autonomatically generate html fils

--- Data Wrangling with MongoDB/Lesson2/request_flights_data.py
+++ Data Wrangling with MongoDB/Lesson2/request_flights_data.py
@@ -1,29 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+from carriers import *
+from airports import *
 
-s = requests.Session()
+html_page = "options.html"
 
-r = s.get("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
-soup = BeautifulSoup(r.text,"lxml")
-viewstate_element = soup.find(id="__VIEWSTATE")
-viewstate = viewstate_element['value']
-eventvalidation_element = soup.find(id="__EVENTVALIDATION")
-eventvalidation = eventvalidation_element["value"]
+carriers = extract_carriers(html_page)
+airports = extract_airports(html_page)
+print carriers, airports
+print len(carriers), len(airports)
 
-viewstategenerator_element = soup.find(id="__VIEWSTATEGENERATOR")
-viewstategenerator = viewstategenerator_element["value"]
+for carrier in carriers[:3]:
 
-r = s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
-           data = (
-                   ("__EVENTTARGET", ""),
-                   ("__EVENTARGUMENT", ""),
-                   ("__VIEWSTATE", viewstate),
-                   ("__VIEWSTATEGENERATOR",viewstategenerator),
-                   ("__EVENTVALIDATION", eventvalidation),
-                   ("CarrierList", "VX"),
-                   ("AirportList", "BOS"),
-                   ("Submit", "Submit")
-                  ))
+    s = requests.Session()
 
-f = open("virgin_and_logan_airport.html", "w")
-f.write(r.text)
+    r = s.get("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
+    soup = BeautifulSoup(r.text,"lxml")
+    viewstate_element = soup.find(id="__VIEWSTATE")
+    viewstate = viewstate_element['value']
+    eventvalidation_element = soup.find(id="__EVENTVALIDATION")
+    eventvalidation = eventvalidation_element["value"]
+
+    viewstategenerator_element = soup.find(id="__VIEWSTATEGENERATOR")
+    viewstategenerator = viewstategenerator_element["value"]
+
+    r = s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+               data = (
+                       ("__EVENTTARGET", ""),
+                       ("__EVENTARGUMENT", ""),
+                       ("__VIEWSTATE", viewstate),
+                       ("__VIEWSTATEGENERATOR",viewstategenerator),
+                       ("__EVENTVALIDATION", eventvalidation),
+                       ("CarrierList", "VX"),
+                       ("AirportList", "BOS"),
+                       ("Submit", "Submit")
+                      ))
+    # carrier = carriers[1]
+    airport = airports[carriers.index(carrier)]
+    f = open("data/"+carrier+'-'+airport+".html", "w")
+    f.write(r.text)
