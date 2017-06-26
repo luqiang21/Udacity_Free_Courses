9019a1202d32339a764d52e384c18119a8275ca9
Qiang Lu <buaaluqiang@hotmail.com>
2017-06-26 12:12:27 -0600

    html_soup.py using beautifulSoup

---
+++ Data Wrangling with MongoDB/Lesson2/html_soup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Please note that the function 'make_request' is provided for your reference only.
+# You will not be able to to actually use it from within the Udacity web UI.
+# Your task is to process the HTML using BeautifulSoup, extract the hidden
+# form field values for "__EVENTVALIDATION" and "__VIEWSTATE" and set the appropriate
+# values in the data dictionary.
+# All your changes should be in the 'extract_data' function
+from bs4 import BeautifulSoup
+import requests
+import json
+
+html_page = "page_source.htm"
+
+def options(soup, id):
+    options_values = []
+    carrier_list = soup.find(id=id)
+    for option in carrier_list.find_all('option'):
+        options_values.append(option['value'])
+    return options_values
+
+def print_list(label, codes):
+    print '\n%s:' % label
+    for c in codes:
+        print c
+
+def extract_data(page):
+    data = {"eventvalidation": "",
+            "viewstate": ""}
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        ev = soup.find(id="__EVENTVALIDATION")
+        data["eventvalidation"] = ev["value"]
+
+        ev = soup.find(id="__VIEWSTATE")
+        data["viewstate"] = ev["value"]
+    return data
+
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+
+    r = requests.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+                    data={'AirportList': "BOS",
+                          'CarrierList': "VX",
+                          'Submit': 'Submit',
+                          "__EVENTTARGET": "",
+                          "__EVENTARGUMENT": "",
+                          "__EVENTVALIDATION": eventvalidation,
+                          "__VIEWSTATE": viewstate
+                    })
+
+    return r.text
+
+
+def test():
+    data = extract_data(html_page)
+    assert data["eventvalidation"] != ""
+    assert data["eventvalidation"].startswith("/wEWjAkCoIj1ng0")
+    assert data["viewstate"].startswith("/wEPDwUKLTI")
+
+    print "succeded."
+
+test()
